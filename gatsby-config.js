c6fe7b3368b28921a8c16c0f04fb235f51d4d259
我是个文盲 <1131310577@qq.com>
2021-11-07 15:38:25 +0800

    style

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,19 @@ module.exports = {
   /* Your site config here */
   plugins: [
     `gatsby-plugin-transition-link`,
-
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/utils`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/log.jpeg`, // This path is relative to the root of the site.
+      },
+    },
   ],
 }
--- src/component/backgroundLayout.js
+++ src/component/backgroundLayout.js
@@ -5,7 +5,7 @@ import InlineCards from "./inlineCards"
 
 export default () => {
 
-  let defaultPath = "/utils/fileInput"
+  let defaultPath = "/fileInput"
 
   return (
     <div className={containerStyles.background}>

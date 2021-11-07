ef61dbe45114ef2518f7cac2d9a095d6014cbdcd
我是个文盲 <1131310577@qq.com>
2021-11-07 16:16:19 +0800

    style

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,22 +5,24 @@
  */
 
 module.exports = {
+  pathPrefix: `/utils`,
+
   /* Your site config here */
   plugins: [
     `gatsby-plugin-transition-link`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `GatsbyJS`,
-        short_name: `GatsbyJS`,
-        start_url: `/utils`,
-        background_color: `#6b37bf`,
-        theme_color: `#6b37bf`,
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: `standalone`,
-        icon: `static/log.jpeg`, // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `GatsbyJS`,
+    //     short_name: `GatsbyJS`,
+    //     start_url: `/`,
+    //     background_color: `#6b37bf`,
+    //     theme_color: `#6b37bf`,
+    //     // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+    //     // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+    //     display: `standalone`,
+    //     icon: `static/log.jpeg`, // This path is relative to the root of the site.
+    //   },
+    // },
   ],
 }

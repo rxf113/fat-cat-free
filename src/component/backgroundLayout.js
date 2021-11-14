d1d2daa5967914699c51e2a80b47772c45141151
我是个文盲 <1131310577@qq.com>
2021-11-14 17:29:01 +0800

    'linkutil'

--- src/component/backgroundLayout.js
+++ src/component/backgroundLayout.js
@@ -11,7 +11,7 @@ export default () => {
     <div className={containerStyles.background}>
       <InlineCards
         left={{ img: "https://rxf113.xyz/static/ptow.jpg", path: defaultPath, title: "pdf转word" }}
-        right={{ img: "https://rxf113.xyz/static/zphd.jpg", path: "/pic/pic-convert.html", title: "照片换底" }}
+        right={{ img: "https://rxf113.xyz/static/zphd.jpg", path: "https://rxf113.xyz/utils/pic/pic-convert.html", title: "照片换底" }}
       />
     </div>)
 }

b20d80864cd8f4131470793511892dd8afdb3ec3
rxf113 <1131310577@qq.com>
2022-01-09 17:18:45 +0800

    update 动态方法

--- src/component/fileLoad.js
+++ src/component/fileLoad.js
@@ -1,53 +1,85 @@
 import React from "react"
 import axios from "axios"
 
-const download = function download(fileId, success, fail) {
-  let options = {
-    method: "GET",
-    responseType: "blob",
-    url: `https://rxf113.xyz/fat-cat/api/download?fileId=${fileId}`
+const getDownloadFunc = function getDownloadFunc(fileId, success, fail, url) {
+
+  return function () {
+    let options = {
+      method: "GET",
+      responseType: "blob",
+      url: `${url}?fileId=${fileId}`
+    }
+    axios(options)
+      .then(res => {
+        success(res)
+      }).catch(error => {
+        fail(error)
+      })
   }
-  axios(options)
-    .then(res => {
-      success(res)
-    }).catch(error => {
-    fail(error)
-  })
 }
 
-const convert = function convert(fileId, success, fail) {
-  let promise = axios(`https://rxf113.xyz/fat-cat/api/convert?fileId=${fileId}`)
-  promise.then(function(response) {
-    success(response)
-  }).catch(function(error) {
-    fail(error)
-  })
-}
 
-const upload = function upload(file, success, fail) {
-  let fd = new FormData()
-  ///let file = document.getElementById("fileInputId").files[0];
-  fd.append("file", file)
-  console.log("==============")
-  console.log(file.path);
-  console.log("==============")
-  const options = {
-    method: "POST",
-    headers: { "content-type": "multipart/form-data"},
-    data: fd,
-    //url: "https://rxf113.xyz/utils/api/upLoadPicture",
-    timeout: 10000,
-    url: "https://rxf113.xyz/fat-cat/api/upload"
+const geUploadFunc = function geUploadFunc(file, success, fail, url) {
+
+  return function () {
+    let fd = new FormData()
+    fd.append("file", file)
+
+    const options = {
+      method: "POST",
+      headers: { "content-type": "multipart/form-data" },
+      data: fd,
+      timeout: 10000,
+      url: url
+    }
+
+    let promise = axios(options)
+    promise.then(function (response) {
+      success(response)
+    }).catch(function (error) {
+      fail(error)
+    })
   }
-
-  let promise = axios(options)
-  promise.then(function(response) {
-    success(response)
-  }).catch(function(error) {
-    fail(error)
-    //success("response")
-  })
 }
 
 
-export { upload, convert, download }
+const getConvertFunc = function getConvertFunc(fileId, success, fail, url) {
+  return function () {
+    let promise = axios(`${url}?fileId=${fileId}`)
+    promise.then(function (response) {
+      success(response)
+    }).catch(function (error) {
+      fail(error)
+    })
+  }
+}
+
+//pdf转word功能 ==========================
+
+const pdf2WordDownload = function (fileId, success, fail) {
+  return getDownloadFunc(fileId, success, fail, "http://192.168.1.5:9999/api/download");
+}
+const pdf2WordUpload = function (file, success, fail) {
+  return geUploadFunc(file, success, fail, "http://192.168.1.5:9999/api/upload");
+}
+const pdf2WordConvert = function (fileId, success, fail) {
+  return getConvertFunc(fileId, success, fail, "http://192.168.1.5:9999/api/convert");
+}
+
+
+const pdf2Word = {
+  upload: pdf2WordUpload,
+  convert: pdf2WordConvert,
+  download: pdf2WordDownload
+}
+
+const type2Methods = {
+  1: pdf2Word
+}
+
+//pdf转word功能 ==========================
+
+
+
+
+export default type2Methods 
